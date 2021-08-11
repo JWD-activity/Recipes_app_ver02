059ec2e7952aab791b8dce7359b119dc3271d1e2
Jinok <joseo46@hotmail.com>
2021-08-11 14:47:38 +1000

    add media queries

--- src/UI/Header.js
+++ src/UI/Header.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Logo from '../components/Logo';
 import Button from '../components/Button';
 import classes from './Header.module.css';
 function Header() {
+  const [button, setButton] = useState(true);
+
+  const showButton = () => {
+    if (window.innerWidth <= 576) setButton(false);
+    else setButton(true);
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener('resize', showButton);
+
   return (
-    <header className='row py-3 align-items-center'>
-      <div className='col-sm-10 col-md-3'>
+    <header className='row py-4 px-3 align-items-center'>
+      <div className='col-sm-4'>
         <Logo />
       </div>
-      <div className='col-sm-10 col-md-4'>search</div>
-      <div className='col-sm-10 col-md-5'>
-        <Button icon='fas fa-plus-square'>Add recipe</Button>
-        <Button icon='fas fa-bookmark'>Bookmark</Button>
+      <div className={`col-sm-8 ${classes['btn-box']}`}>
+        <Button icon='fas fa-plus-square'>{button && 'Add recipe'}</Button>
+        <Button icon='fas fa-bookmark'>{button && 'Bookmark'}</Button>
       </div>
     </header>
   );
