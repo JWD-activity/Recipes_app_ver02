f3f4ea09cc77777160914b55bb3cf389d77a90fb
Jinok <joseo46@hotmail.com>
2021-08-15 14:32:57 +1000

    add id generator

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ function App() {
     setRecipeList((prevRecipeList) => {
       return [...prevRecipeList, recipe];
     });
+
+    console.log(recipeList);
   };
 
   const onClickHanlder = () => {
