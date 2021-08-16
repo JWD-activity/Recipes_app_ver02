ce6cf254daae455da074e5f116f13a362c43add6
Jinok <joseo46@hotmail.com>
2021-08-16 20:53:35 +1000

    add time convert function

--- src/components/Recipes/RecipeDetail.js
+++ src/components/Recipes/RecipeDetail.js
@@ -6,13 +6,22 @@ function RecipeDetail(props) {
   const id = props.clickedRecipe;
   const recipe = props.recipes.find((recipe) => recipe.id === id);
 
+  const calcCookingTime = (time) => {
+    if (time >= 60) {
+      let hours = Math.floor(time / 60);
+      var min = time % 60;
+      return hours + 'hr ' + min + 'mins';
+    }
+  };
+
   return (
     <>
       {recipe ? (
         <ul key={recipe.id} className={`row ${classes.ul}`}>
           <li>{recipe.title}</li>
           <li className={'col'}>
-            <i className='far fa-clock'></i> {recipe.cookingTime}min
+            <i className='far fa-clock'></i>{' '}
+            {calcCookingTime(+recipe.cookingTime)}
           </li>
           <li className={'col'}>
             <i className='fas fa-user-friends'></i>
