8a5d03c7a96bb1e592f11fe2d90bb0ad44aa03af
Jinok <joseo46@hotmail.com>
2021-08-17 11:15:37 +1000

    add datset attribute for ingredients

--- src/components/Recipes/IngrediList.js
+++ src/components/Recipes/IngrediList.js
@@ -2,17 +2,20 @@ import React from 'react';
 
 function IngrediList(props) {
   let list = props.listOfIngredient;
-
+  let count = 0;
   return list.length !== 0
     ? list.map((ingredient) => (
         <li
-          key={ingredient}
+          key={count++}
           data-id={ingredient}
+          data-index={count}
           className={`${props.className ?? ''}`}
           onClick={props.deleteHandler}
         >
           {ingredient}
-          {props.icon && <i data-id={ingredient} className={props.icon} />}
+          {props.icon && (
+            <i data-index={count} data-id={ingredient} className={props.icon} />
+          )}
         </li>
       ))
     : '';
